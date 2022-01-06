bfe119068df5c44f70cf5d353d3c957c17109c1e
Peyo Tanchev <peyotanchev@gmail.com>
2022-01-06 13:07:40 +0200

    add Enter event in js file

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let weather = {
     "apiKey": "f2c5584a2733ba498888a70e018137da",
     fetchWeather: function(city) {
-        fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q=" 
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
             + "&units=metric&appid=" 
             + this.apiKey
@@ -12,9 +11,26 @@ let weather = {
     },
     displayWeather: function(data) {
         const { name } = data;
-        const { icon, description } = data.weather;
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name, icon,description,temp,humidity,speed)
-    }
-}
+        document.querySelector(".city").innerText = "Whether in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity is" + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind is" + speed + "km/h";
+    },
+search: function (){
+    this.fetchWeather(document.querySelector(".search-bar").value)
+},
+};
+document.querySelector(".search button").addEventListener("click", function(){
+weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+  if (event.key == "Enter") {
+   weather.search();   
+  }
+})
